82dcd49b5e3b851233efce3e7f1762c3d1b8c0f4
Par <pacroy@gmail.com>
2020-08-19 13:45:27 +0700

    Write program to display ip address

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace ipcheck
+public class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
+    public static void Main(string[] args)
+    {        
+        // Check if network is available
+        if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Current IP Addresses:");
+
+            // Get host entry for current hostname
+            string hostname = System.Net.Dns.GetHostName();
+            System.Net.IPHostEntry host = System.Net.Dns.GetHostEntry(hostname);
+                
+            // Iterate over each IP address and render their values
+            foreach(System.Net.IPAddress address in host.AddressList)
+            {
+                System.Console.WriteLine($"\t{address}");
+            }
+        }
+        else
+        {
+            System.Console.WriteLine("No Network Connection");
         }
     }
 }
