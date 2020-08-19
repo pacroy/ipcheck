26e37add877fa74d99d63e8025a0b194ae2b8162
Par <pacroy@gmail.com>
2020-08-19 13:36:19 +0700

    Generate from dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ipcheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
